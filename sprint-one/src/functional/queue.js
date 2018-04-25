d04e82d662aeddb043c4fb9fbb6ce7ebd9743796
Elizabeth Li <sincerelylibby@gmail.com>
2018-04-25 14:51:50 -0700

    Passed tests for the functional pattern of queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 1;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    var leastRecent = Math.min(...Object.keys(storage));
+    var dequeuedItem = storage[leastRecent];
+    delete storage[leastRecent];
+    return dequeuedItem;
   };
 
   someInstance.size = function() {
+    storage.size = Object.keys(storage).length;
+    return storage.size;
   };
 
   return someInstance;
