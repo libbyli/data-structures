9a970e3034777ff6d1c41fe32900fad9d44cc497
Elizabeth Li <sincerelylibby@gmail.com>
2018-04-25 16:15:51 -0700

    Passed tests for the prototypal pattern of stack and queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.counter = 1;
+
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.counter] = value;
+  this.counter++;
+};
+
+queueMethods.dequeue = function() {
+  var leastRecent = Math.min(...Object.keys(this.storage));
+  var dequeuedItem = this.storage[leastRecent];
+  delete this.storage[leastRecent];
+  return dequeuedItem;
+};
+
+queueMethods.size = function() {
+  this.storage.size = Object.keys(this.storage).length;
+  return this.storage.size;
+};
 
 
