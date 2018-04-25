b793872724d7032c19c60f09bdf7459206355292
Elizabeth Li <sincerelylibby@gmail.com>
2018-04-25 13:27:34 -0700

    Passed tests for the functional pattern of stack.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 1;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    var mostRecent = Object.keys(storage).length;
+    var poppedItem = storage[mostRecent];
+    delete storage[mostRecent];
+    counter--;
+    return poppedItem;
   };
 
   someInstance.size = function() {
+    storage.size = Object.keys(storage).length;
+    return storage.size;
   };
 
   return someInstance;
