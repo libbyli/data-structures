cb3c61d09ea5b64922531401a4a355c949d45074
libbyli <sincerelylibby@gmail.com>
2018-04-26 10:52:40 -0700

    Passed tests for linkedList.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    };
+
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var oldHead = list.head.value;
+      list.head = list.head.next;
+      return oldHead;
+    } else {
+      return null; 
+    }
   };
 
   list.contains = function(target) {
+    var findTarget = function(node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        if (node.next === null) {
+          return false;
+        } else {
+          return findTarget(node.next);
+        }
+      }
+    }
+    return findTarget(list.head);
   };
 
   return list;
