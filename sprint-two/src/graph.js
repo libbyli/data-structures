e1a97c23e72f20451b05a5b7d438fdc6278086c1
libbyli <sincerelylibby@gmail.com>
2018-04-26 18:26:09 -0700

    Passed first four tests for graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -15,7 +15,7 @@ Graph.prototype.findNodeIndex = function(node) {
     counter++;
   }
   return index;
-}
+};
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
@@ -42,15 +42,34 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // use findNodeIndex to find the nodes in storage
+  // check the edges array of fromNode object to see if it contains toNode object 
+  // (don't need to check vice versa because it's an undirected graph and the relationship is symmetrical)
+  // return true if it does and false it if doesn't 
+  var index = this.findNodeIndex(fromNode);
+  var nodeEdges = this.storage[index].edges;
+  for(var i = 0; i < nodeEdges.length; i++){
+    if(nodeEdges[i] === toNode){
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  
+  // use findNodeIndex to find the nodes in storage
+  // push toNode object into the edge array in fromNode object and vice versa
+  var indexFromNode = this.findNodeIndex(fromNode);
+  var indexToNode = this.findNodeIndex(toNode);
+  this.storage[indexFromNode].edges.push(toNode);
+  this.storage[indexToNode].edges.push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  // use findNodeIndex to find the nodes in storage
+  // splice the edge array of fromNode object to remove toNode object and vice versa
 };
 
 // Pass in a callback which will be executed on each node of the graph.
