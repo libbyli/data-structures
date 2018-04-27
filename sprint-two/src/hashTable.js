793b619c6fd91d219f45d2e67133962132da76bb
libbyli <sincerelylibby@gmail.com>
2018-04-27 11:25:20 -0700

    Worked on hash table.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,36 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._storage.each(function(bucket, index, storage) {
+    this._storage.set(index, []);
+  });
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var found = false;
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v;
+      found = true;
+    } 
+  }
+  if (found === false) {
+    bucket.push([k, v]);
+    this._storage.set(index, bucket);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
