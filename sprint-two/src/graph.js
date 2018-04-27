93c86dcbe8af574ef2a853e341f9dcb15ee488bf
libbyli <sincerelylibby@gmail.com>
2018-04-26 20:39:26 -0700

    Passed all tests for graph.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -38,18 +38,25 @@ Graph.prototype.contains = function(node) {
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
   this.storage.splice(this.findNodeIndex(node), 1);
+  for (var j = 0; j < this.storage.length; j++) {
+    for (var i = 0; i < this.storage[j].edges.length; i++){
+      if (this.storage[j].edges[i] === node) {
+        this.storage[j].edges.splice(i, 1);
+      }
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
   // use findNodeIndex to find the nodes in storage
-  // check the edges array of fromNode object to see if it contains toNode object 
+  // check the edges array of fromNode object to see if it contains toNode  
   // (don't need to check vice versa because it's an undirected graph and the relationship is symmetrical)
   // return true if it does and false it if doesn't 
   var index = this.findNodeIndex(fromNode);
   var nodeEdges = this.storage[index].edges;
-  for(var i = 0; i < nodeEdges.length; i++){
-    if(nodeEdges[i] === toNode){
+  for (var i = 0; i < nodeEdges.length; i++) {
+    if (nodeEdges[i] === toNode) {
       return true;
     }
   }
@@ -59,7 +66,7 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   // use findNodeIndex to find the nodes in storage
-  // push toNode object into the edge array in fromNode object and vice versa
+  // push toNode into the edge array in fromNode and vice versa
   var indexFromNode = this.findNodeIndex(fromNode);
   var indexToNode = this.findNodeIndex(toNode);
   this.storage[indexFromNode].edges.push(toNode);
@@ -69,15 +76,39 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   // use findNodeIndex to find the nodes in storage
-  // splice the edge array of fromNode object to remove toNode object and vice versa
+  // splice the edge array of fromNode object to remove toNode and vice versa
+  var indexFromNode = this.findNodeIndex(fromNode);
+  var indexToNode = this.findNodeIndex(toNode);
+  for (var i = 0; i < this.storage[indexFromNode].edges.length; i++){
+    if (this.storage[indexFromNode].edges[i] === toNode) {
+      this.storage[indexFromNode].edges.splice(i, 1);
+    }
+  }
+  for (var i = 0; i < this.storage[indexToNode].edges.length; i++){
+    if (this.storage[indexToNode].edges[i] === fromNode) {
+      this.storage[indexToNode].edges.splice(i, 1);
+    }
+  }
+  
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var i = 0; i < this.storage.length; i++) {
+    cb(this.storage[i].value);
+  }
+  
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   Graph.prototype.addNode is O(1), constant.
+   Graph.prototype.contains is O(n), linear.
+   Graph.prototype.removeNode is O(n squared), quadratic.
+   Graph.prototype.hasEdge is O(n), linear.
+   Graph.prototype.addEdge is O(n), linear.
+   Graph.prototype.removeEdge is O(n), linear.
+   Graph.prototype.forEachNode is O(n), linear.
  */
 
 
